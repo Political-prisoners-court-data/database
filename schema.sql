052248cf79353a06577da54e800613166a3c4805
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-07-04 19:11:22 +0200

    Ordered SQL statements to initiate db without errors

--- schema.sql
+++ schema.sql
@@ -21,35 +21,6 @@ CREATE TABLE db.rfm_person
     PRIMARY KEY (full_name, birth_date)
 );
 
-CREATE OR REPLACE VIEW db.rfm_added AS
-SELECT new.full_name, new.birth_date, new.is_terr
-FROM out.rfm_person AS old
-         RIGHT OUTER JOIN db.rfm_person AS new
-                          USING (full_name, birth_date)
-WHERE old.full_name IS NULL;
-
-CREATE OR REPLACE VIEW db.rfm_removed AS
-SELECT old.full_name, old.birth_date, old.is_terr
-FROM out.rfm_person AS old
-         LEFT OUTER JOIN db.rfm_person AS new
-                         USING (full_name, birth_date)
-WHERE new.full_name IS NULL;
-
-CREATE OR REPLACE VIEW db.rfm_changed AS
-SELECT old.full_name,
-       old.birth_date,
-       old.is_terr as old_is_terr,
-       new.is_terr as new_is_terr,
-       old.aliases as old_aliases,
-       new.aliases as new_aliases,
-       old.address as old_address,
-       new.address as new_address
-FROM out.rfm_person AS old
-         INNER JOIN db.rfm_person AS new
-                    USING (full_name, birth_date)
-WHERE old.is_terr <> new.is_terr
-   OR old.address <> new.address
-   OR old.aliases <> new.aliases;
 
 GRANT TRUNCATE, SELECT, INSERT ON ALL TABLES IN SCHEMA db to db;
 
@@ -97,7 +68,37 @@ create table out.rfm_changed
     new_address text
 );
 
-GRANT INSERT ON ALL TABLES IN SCHEMA out TO db;
+GRANT TRUNCATE, SELECT, INSERT ON ALL TABLES IN SCHEMA out to db;
+
+CREATE OR REPLACE VIEW db.rfm_added AS
+SELECT new.full_name, new.birth_date, new.is_terr
+FROM out.rfm_person AS old
+         RIGHT OUTER JOIN db.rfm_person AS new
+                          USING (full_name, birth_date)
+WHERE old.full_name IS NULL;
+
+CREATE OR REPLACE VIEW db.rfm_removed AS
+SELECT old.full_name, old.birth_date, old.is_terr
+FROM out.rfm_person AS old
+         LEFT OUTER JOIN db.rfm_person AS new
+                         USING (full_name, birth_date)
+WHERE new.full_name IS NULL;
+
+CREATE OR REPLACE VIEW db.rfm_changed AS
+SELECT old.full_name,
+       old.birth_date,
+       old.is_terr as old_is_terr,
+       new.is_terr as new_is_terr,
+       old.aliases as old_aliases,
+       new.aliases as new_aliases,
+       old.address as old_address,
+       new.address as new_address
+FROM out.rfm_person AS old
+         INNER JOIN db.rfm_person AS new
+                    USING (full_name, birth_date)
+WHERE old.is_terr <> new.is_terr
+   OR old.address <> new.address
+   OR old.aliases <> new.aliases;
 
 CREATE VIEW out.match_rfm_full_name AS
 SELECT *
