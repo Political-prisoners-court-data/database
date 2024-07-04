f88b84484e5b2ec1586d6b926319e9687e3d0d98
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-07-04 19:12:37 +0200

    Truncate old.* tables before filling with new data

--- ingest_rfm.py
+++ ingest_rfm.py
@@ -5,6 +5,10 @@ from sqlalchemy import create_engine, text
 engine = create_engine('postgresql://db@localhost:5432/postgres')
 
 with engine.begin() as connection:
+    connection.execute(text('TRUNCATE TABLE out.rfm_added'))
+    connection.execute(text('TRUNCATE TABLE out.rfm_removed'))
+    connection.execute(text('TRUNCATE TABLE out.rfm_changed'))
+    connection.execute(text('TRUNCATE TABLE out.rfm_person'))
     connection.execute(text('INSERT INTO out.rfm_added SELECT * from db.rfm_added'))
     connection.execute(text('INSERT INTO out.rfm_removed SELECT * from db.rfm_removed'))
     connection.execute(text('INSERT INTO out.rfm_changed SELECT * from db.rfm_changed'))
